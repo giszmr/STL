ec42bfe1510fecd2ab01a9db684ee10e64f8dd6f
giszmr <gismengri@163.com>
2019-04-12 16:59:14 +0800

    daily pratice

--- STL_Proj/STL_Proj/list_test.cpp
+++ STL_Proj/STL_Proj/list_test.cpp
@@ -311,11 +311,33 @@ void ListTest::ModifiersTest()
 }
 
 
-// used for remove_if()
+
+
 // a predicate implemented as a function:
 bool single_digit(const int& value) 
 {
 	return value < 10; 
+}
+bool gap_no_less_than(const double& first, const double& second)
+{
+	return fabs(first - second) < 2.0;
+}
+bool compare_double(const double& first, const double& second)
+{
+	return first < second; //why only this pattern?????
+}
+bool compare_nocase(const std::string& first, const std::string& second)
+{
+	unsigned int i = 0;
+	while ((i < first.length()) && (i < second.length()))
+	{
+		if (tolower(first[i]) < tolower(second[i]))
+			return true;
+		else if (tolower(first[i]) > tolower(second[i]))
+			return false;
+		++i;
+	}
+	return (first.length() < second.length());
 }
 // a predicate implemented as a class:
 struct is_odd
@@ -325,6 +347,16 @@ struct is_odd
 		return (value % 2) == 1; 
 	}
 };
+struct is_near
+{
+	bool operator() (const int& first, const int& second)
+	{
+		return fabs(first - second) < 2;
+	}
+};
+//A predicate is a C++ function returning a boolean or an object having a bool operator() member.
+//A unary predicate takes one argument, a binary takes two, and so on.
+
 
 void ListTest::OperationsTest()
 {
@@ -434,6 +466,7 @@ void splice (const_iterator position, list&& x,
 	std::cout << "second  " << second.empty() << "      " << second.size() << "     " << std::hex << second.max_size() << std::dec << std::endl;
 
 
+
 	std::cout << "---second.remove_if();---" << std::endl;
 	std::list<int> third = { 1,2,3, 40, 50,60, 777,888,999 };
 	std::cout << "third: ";
@@ -443,6 +476,10 @@ void splice (const_iterator position, list&& x,
 	std::cout << "       empty  " << "size  " << "max_size" << std::endl;
 	std::cout << "third  " << third.empty() << "      " << third.size() << "     " << std::hex << third.max_size() << std::dec << std::endl;
 	
+/*
+template <class Predicate>
+	void remove_if (Predicate pred);
+*/
 	std::cout << "---second.remove_if(single_digit);---" << std::endl;
 	third.remove_if(single_digit);
 	std::cout << "third: ";
@@ -461,5 +498,202 @@ void splice (const_iterator position, list&& x,
 	std::cout << "       empty  " << "size  " << "max_size" << std::endl;
 	std::cout << "third  " << third.empty() << "      " << third.size() << "     " << std::hex << third.max_size() << std::dec << std::endl;
 
+
+/*
+(1)	
+void unique();
+
+(2)	
+template <class BinaryPredicate>
+	void unique (BinaryPredicate binary_pred);
+*/
+	std::cout << "---std::list.unique();---" << std::endl;
+	std::list<int> fourth = { 1,1,2,2,3,3,4,4,7,6,8,13,13,11,12 };
+	std::list<double> fifth = { 1.1,2.2,2.2,3.3,3.3,4.4,5.5,7.7 };
+	std::list<std::string> sixth = { "one","one","two","three","four","four" };
+	std::cout << "-before:" << std::endl;
+	std::cout << "fourth: ";
+	for (auto it = fourth.cbegin(); it != fourth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "fifth : ";
+	for (auto it = fifth.cbegin(); it != fifth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "sixth : ";
+	for (auto it = sixth.cbegin(); it != sixth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "fourth  " << fourth.empty() << "      " << fourth.size() << "    " << std::hex << fourth.max_size() << std::dec << std::endl;
+	std::cout << "fifth   " << fifth.empty() << "      " << fifth.size() << "     " << std::hex << fifth.max_size() << std::dec << std::endl;
+	std::cout << "sixth   " << sixth.empty() << "      " << sixth.size() << "     " << std::hex << sixth.max_size() << std::dec << std::endl;
+
+	std::cout << "-after:" << std::endl;
+	fourth.unique();
+	fifth.unique();
+	sixth.unique();
+	std::cout << "fourth: ";
+	for (auto it = fourth.cbegin(); it != fourth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "fifth : ";
+	for (auto it = fifth.cbegin(); it != fifth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "sixth : ";
+	for (auto it = sixth.cbegin(); it != sixth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "fourth  " << fourth.empty() << "      " << fourth.size() << "    " << std::hex << fourth.max_size() << std::dec << std::endl;
+	std::cout << "fifth   " << fifth.empty() << "      " << fifth.size() << "     " << std::hex << fifth.max_size() << std::dec << std::endl;
+	std::cout << "sixth   " << sixth.empty() << "      " << sixth.size() << "     " << std::hex << sixth.max_size() << std::dec << std::endl;
+
+	std::cout << "---std::list.unique(binary_pred);---" << std::endl;
+	fourth.unique(is_near());
+	fifth.unique(gap_no_less_than);
+	std::cout << "fourth: ";
+	for (auto it = fourth.cbegin(); it != fourth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "fifth : ";
+	for (auto it = fifth.cbegin(); it != fifth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "fourth  " << fourth.empty() << "      " << fourth.size() << "     " << std::hex << fourth.max_size() << std::dec << std::endl;
+	std::cout << "fifth   " << fifth.empty() << "      " << fifth.size() << "     " << std::hex << fifth.max_size() << std::dec << std::endl;
+
+
+
+/*
+(1)	
+  void merge (list& x);
+  void merge (list&& x);
+
+(2)	
+template <class Compare>
+  void merge (list& x, Compare comp);
+template <class Compare>
+  void merge (list&& x, Compare comp);
+
+  Merges x into the list by transferring all of its elements
+  at their respective ordered positions
+  into the container (both containers shall already be ordered).
+ */
+	std::cout << "---std::list.merge();---" << std::endl;
+	std::list<double> seventh = { 3.9, 8.7, 2.1 };
+	std::list<double> eighth = { 4.2, 5.9, 3.4, 6.7 };
+	std::cout << "-raw data:" << std::endl;
+	std::cout << "seventh: ";
+	for (auto it = seventh.cbegin(); it != seventh.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "eighth : ";
+	for (auto it = eighth.cbegin(); it != eighth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "seventh  " << seventh.empty() << "      " << seventh.size() << "     " << std::hex << seventh.max_size() << std::dec << std::endl;
+	std::cout << "eighth   " << eighth.empty() << "      " << eighth.size() << "     " << std::hex << eighth.max_size() << std::dec << std::endl;
+
+	std::cout << "---std::list.sort();---" << std::endl;
+	seventh.sort();
+	eighth.sort();
+	std::cout << "seventh: ";
+	for (auto it = seventh.cbegin(); it != seventh.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "eighth : ";
+	for (auto it = eighth.cbegin(); it != eighth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "seventh  " << seventh.empty() << "      " << seventh.size() << "     " << std::hex << seventh.max_size() << std::dec << std::endl;
+	std::cout << "eighth   " << eighth.empty() << "      " << eighth.size() << "     " << std::hex << eighth.max_size() << std::dec << std::endl;
+
+	std::cout << "---std::list.merge(list& x);---" << std::endl;
+	seventh.merge(eighth); //If both lists are not sorted, program will crash.
+	std::cout << "seventh: ";
+	for (auto it = seventh.cbegin(); it != seventh.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "eighth : ";
+	for (auto it = eighth.cbegin(); it != eighth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "seventh  " << seventh.empty() << "      " << seventh.size() << "     " << std::hex << seventh.max_size() << std::dec << std::endl;
+	std::cout << "eighth   " << eighth.empty() << "      " << eighth.size() << "     " << std::hex << eighth.max_size() << std::dec << std::endl;
+
+	/*
+	Compare comp: Binary predicate
+	*/
+	std::cout << "---std::list.merge(list& x, Compare comp);---" << std::endl;
+	eighth.merge(seventh, compare_double); //If both lists are not sorted, program will crash.
+	std::cout << "seventh: ";
+	for (auto it = seventh.cbegin(); it != seventh.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "eighth : ";
+	for (auto it = eighth.cbegin(); it != eighth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "seventh  " << seventh.empty() << "      " << seventh.size() << "     " << std::hex << seventh.max_size() << std::dec << std::endl;
+	std::cout << "eighth   " << eighth.empty() << "      " << eighth.size() << "     " << std::hex << eighth.max_size() << std::dec << std::endl;
+
+
+	std::cout << "---std::list.reverse();---" << std::endl;
+	eighth.reverse();
+	std::cout << "eighth : ";
+	for (auto it = eighth.cbegin(); it != eighth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "eighth   " << eighth.empty() << "      " << eighth.size() << "     " << std::hex << eighth.max_size() << std::dec << std::endl;
+	std::cout << "---std::list.reverse();---" << std::endl;
+	eighth.reverse();
+	std::cout << "eighth : ";
+	for (auto it = eighth.cbegin(); it != eighth.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "eighth   " << eighth.empty() << "      " << eighth.size() << "     " << std::hex << eighth.max_size() << std::dec << std::endl;
+
+
+
+	std::cout << "---std::list.sort(); raw data---" << std::endl;
+	std::list<std::string> mylist = { "one", "two", "three", "four" };
+	std::cout << "mylist: ";
+	for (auto it = mylist.cbegin(); it != mylist.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "mylist  " << mylist.empty() << "      " << mylist.size() << "     " << std::hex << mylist.max_size() << std::dec << std::endl;
+
+	std::cout << "---std::list.sort();---" << std::endl;
+	mylist.sort();
+	std::cout << "mylist: ";
+	for (auto it = mylist.cbegin(); it != mylist.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "mylist  " << mylist.empty() << "      " << mylist.size() << "     " << std::hex << mylist.max_size() << std::dec << std::endl;
+
+	/*
+comp
+	Binary predicate that, taking two values of the same type of those contained in the list, returns true if the first argument goes before the second argument in the strict weak ordering it defines, and false otherwise.
+	This shall be a function pointer or a function object.
+	*/
+	std::cout << "---std::list.sort(Compare comp);---" << std::endl;
+	mylist.sort(compare_nocase);
+	std::cout << "mylist: ";
+	for (auto it = mylist.cbegin(); it != mylist.cend(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << "        empty  " << "size  " << "max_size" << std::endl;
+	std::cout << "mylist  " << mylist.empty() << "      " << mylist.size() << "     " << std::hex << mylist.max_size() << std::dec << std::endl;
 
 }
